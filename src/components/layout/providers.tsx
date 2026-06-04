d8a2a39322d0dc22005ac5fded6940a14387ef6e
mahesgya <anargya115@gmail.com>
2026-06-04 23:06:22 +0700

    feat(providers): add react-hot-toast toaster

--- src/components/layout/providers.tsx
+++ src/components/layout/providers.tsx
@@ -1,13 +1,19 @@
 'use client'
 
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
 
 const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const content = <Provider store={store}>{children}</Provider>
+  const content = (
+    <Provider store={store}>
+      {children}
+      <Toaster position="top-center" />
+    </Provider>
+  )
 
   if (!googleClientId) return content
 
